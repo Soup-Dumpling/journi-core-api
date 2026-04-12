2373104f6b0e4f52bf479e10411b282ecf552020
Soup-Dumpling <cantoney_wong@hotmail.com>
2026-04-12 02:42:18 +0100

    Database connection setup and seeded headphone data

--- src/CodingChallenge/CodingChallenge.Infrastructure/CodingChallengeDbContext.cs
+++ src/CodingChallenge/CodingChallenge.Infrastructure/CodingChallengeDbContext.cs
@@ -1,21 +1,17 @@
-﻿//using Journi.CodingChallenge.Core.Models.Entities;
-//using Journi.CodingChallenge.Infrastructure.Mappings;
+﻿using Journi.CodingChallenge.Core.Models.Entities;
+using Journi.CodingChallenge.Infrastructure.Mappings;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Journi.CodingChallenge.Infrastructure
 {
     public class CodingChallengeDbContext : DbContext
     {
         public CodingChallengeDbContext(DbContextOptions<CodingChallengeDbContext> contextOptions) : base(contextOptions) { }
-
+        public DbSet<Headphone> Headphones { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new HeadphoneMapping());
         }
     }
 }
